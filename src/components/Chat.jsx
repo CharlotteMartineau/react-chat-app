750a0bf328fa359ec11cc218a8b86bdd0c5b69c4
CharlotteMartineau <charlotte_martineau@hotmail.fr>
2024-03-22 19:56:17 +0100

    redirect to showChatroom if chatrooms

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,11 +2,14 @@ import React, { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getChatroomsRequest } from "../redux/ChatroomsRedux";
 import ChatMenu from "./ChatMenu";
+import { Navigate } from "react-router-dom";
 
 const Chat = () => {
   const dispatch = useDispatch();
   const shouldGetChatrooms = useRef(true);
   const chatrooms = useSelector((state) => state.chatrooms.chatrooms);
+  const hasChatroom = chatrooms.length > 0;
+  const firstChatroomId = hasChatroom && chatrooms[0]?.id;
 
   useEffect(() => {
     if (shouldGetChatrooms.current) {
@@ -16,7 +19,12 @@ const Chat = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <ChatMenu chatrooms={chatrooms} />;
+  return (
+    <>
+      {hasChatroom && <Navigate to={`/chatrooms/${firstChatroomId}`} />}
+      <ChatMenu chatrooms={chatrooms} />
+    </>
+  );
 };
 
 export default Chat;
